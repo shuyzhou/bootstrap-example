fc049812f0ca3edc28feaa6e25ff9f06643a9707
shuyzhou <big_zhou@126.com>
2017-06-27 13:59:16 +0800

    compile less file by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp        = require('gulp');
+var path        = require('path');
+var less        = require('gulp-less');
 var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
 // 静态服务器
@@ -8,6 +10,13 @@ gulp.task('serve', function() {
             baseDir: "./"
         }
     });
-    gulp.watch("*.html").on('change', reload);
+    gulp.watch(["*.html",'./bootstrap/css/*.css']).on('change', reload);
+    gulp.watch("./less/*.less",['less']);
+});
+//编译Less文件
+gulp.task('less', function () {
+  return gulp.src('./less/bootstrap.less')
+    .pipe(less())
+    .pipe(gulp.dest('./bootstrap/css'));
 });
 gulp.task('default', ['serve']);
